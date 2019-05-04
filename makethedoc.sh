38878ede8d72d468a1a0a228635b302f91872370
highlando <jnhlnd@gmail.com>
2019-05-04 06:50:48 +0200

    cool rjs style

--- makethedoc.sh
+++ makethedoc.sh
@@ -1,5 +1,5 @@
 # pandoc -o concept.pdf concept.md
 # pandoc -t revealjs -s -o concept.html concept.md -V revealjs-url=~/software/other/reveal.js 
 # pandoc -t revealjs -s -o myslides.html concept.md --mathml -V revealjs-url=/home/heiland/software/other/reveal.js 
-# pandoc concept.md -o myslides.html -t revealjs -s -H <(echo '\['; cat def.tex; echo '\]')  --mathjax -V revealjs-url=/home/heiland/software/other/reveal.js 
-pandoc concept.md -t beamer -o concept-slides.pdf -H def.tex
+pandoc concept.md -o concept-slides.html -t revealjs -s -H <(echo '\['; cat def.tex; echo '\]')  --mathjax -V revealjs-url=/home/heiland/software/other/reveal.js -V theme=solarized
+# pandoc concept.md -t beamer -o concept-slides.pdf -H def.tex
